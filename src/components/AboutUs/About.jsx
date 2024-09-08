a27f8a80967b7fca9db8100efb1aba7b0fdb7aa5
Muhammad Muzzammil <muzzammil759@gmail.com>
2024-09-08 21:34:33 +0500

    About us section added suucesfullt with proper images setup and also nav links added

--- src/components/AboutUs/About.jsx
+++ src/components/AboutUs/About.jsx
@@ -1,12 +1,15 @@
 const About = () => {
   return (
-    <section className="py-20 bg-gray-100 w-screen overflow-hidden">
+    <section
+      id="about"
+      className="py-20 bg-primary w-screen overflow-hidden font-Inter"
+    >
       <div className="container mx-auto px-4">
         <div className="text-center mb-12">
-          <h2 className="text-4xl lg:text-5xl font-bold text-gray-800 mb-6">
+          <h2 className="text-4xl lg:text-5xl font-bold text-white mb-6">
             About Us
           </h2>
-          <p className="text-lg lg:text-xl text-gray-600 max-w-4xl mx-auto">
+          <p className="text-lg lg:text-xl text-white max-w-4xl mx-auto">
             Welcome to DevYaarn, where we are committed to delivering
             high-quality solutions tailored to meet your unique needs. Our team
             of experts combines years of experience with innovative thinking to
@@ -19,14 +22,14 @@ const About = () => {
           <div className="w-full max-w-md p-4">
             <div className="bg-white shadow-lg p-8 text-center rounded-lg h-full flex flex-col justify-between">
               <img
-                src="/src/assets/feature-icon1.png"
+                src="/images/mission.png"
                 alt="Feature 1"
                 className="mx-auto mb-6 w-24 h-24 lg:w-32 lg:h-32"
               />
-              <h3 className="text-2xl lg:text-3xl font-semibold text-gray-800 mb-3">
+              <h3 className="text-2xl lg:text-3xl font-semibold text-black mb-3">
                 Our Mission
               </h3>
-              <p className="text-gray-600 text-base lg:text-lg">
+              <p className="text-black text-base lg:text-lg">
                 To provide the best solutions that empower businesses to succeed
                 in a competitive market.
               </p>
@@ -35,7 +38,7 @@ const About = () => {
           <div className="w-full max-w-md p-4">
             <div className="bg-white shadow-lg p-8 text-center rounded-lg h-full flex flex-col justify-between">
               <img
-                src="/src/assets/feature-icon2.png"
+                src="/images/vision.png"
                 alt="Feature 2"
                 className="mx-auto mb-6 w-24 h-24 lg:w-32 lg:h-32"
               />
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -3,14 +3,14 @@ const Services = () => {
   let services = [
     {
       id: 1,
-      path: "/images/service.png",
+      path: "/images/web.png",
       title: "Web Development",
       description:
         "We build websites that serve as powerful marketing tools and bring memorable brand experiences.",
     },
     {
       id: 2,
-      path: "/images/service.png",
+      path: "/images/App.png",
       title: "Graphic Design",
       description:
         "We deliver eye-catching, memorable experiences that connect with people, just like you.",
@@ -18,37 +18,37 @@ const Services = () => {
     {
       id: 3,
       title: "Digital Marketing",
-      path: "/images/service.png",
+      path: "/images/Marketing.png",
       description:
         "Digital marketing is the component of marketing that utilizes internet and online based digital technologies such as desktop computers, mobile phones and other digital media and platforms to promote products and services.",
     },
     {
       id: 4,
       title: "Content Creation",
-      path: "/images/service.png",
+      path: "/images/ContentWritting.png",
       description:
         "Content creation is the contribution of information to any media and most especially to digital media for an end-user/audience in specific contexts.",
     },
     {
       id: 5,
-      title: "SEO",
-      path: "/images/service.png",
+      title: "Video Editing",
+      path: "/images/VideoEditing.png",
       description:
-        "Search engine optimization is the process of improving the quality and quantity of website traffic to a website or a web page from search engines.",
+        "Video editing is the manipulation and arrangement of video shots. Video editing is used to structure and present all video information, including films and television shows, video advertisements and video essays.",
     },
     {
       id: 6,
-      title: "App Development",
-      path: "/images/service.png",
+      title: "Graphic Design",
+      path: "/images/graphic.png",
       description:
-        "App development is the process by which a mobile app is developed for mobile devices, such as personal digital assistants, enterprise digital assistants or mobile phones.",
+        "Graphic design is a craft where professionals create visual content to communicate messages. By applying visual hierarchy and page layout techniques, designers use typography and pictures to meet users'.",
     },
   ];
 
   return (
     <>
-      <section id="services" className="mt-12 mb-8 p-4 ">
-        <h1 className="text-3xl text-center text-black/70  font-bold tracking-wide mb-8">
+      <section id="services" className="mt-2 mb-8 p-4 bg-white font-Inter">
+        <h1 className="text-4xl lg:text-5xl text-center text-secondary  font-bold tracking-wide mb-8">
           Services
         </h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-3 place-items-center gap-8">
