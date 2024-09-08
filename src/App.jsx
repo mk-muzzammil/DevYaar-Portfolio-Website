653872f6b05137f1b95505ad7f3395d50654b66e
Muhammad Muzzammil <muzzammil759@gmail.com>
2024-09-08 23:45:05 +0500

    Faqs developed with responsivess

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import Services from "./components/services/services";
 import About from "./components/AboutUs/About";
 import WhyUs from "./components/WhyUS/WhyUs";
 import DiscussProject from "./components/DiscussProjects/DiscussProject";
+import Faqs from "./components/FAQs/Faqs";
 function App() {
   return (
     <>
-      <div className="">
+      <div className="overflow-x-hidden">
         <HeroSection />
         <Services />
         <About />
         <WhyUs />
         <DiscussProject />
+        <Faqs />
       </div>
     </>
   );
