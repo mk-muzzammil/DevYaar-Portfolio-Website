09dd1d25b6e4f6d81f8bda5c53af12c0a5a12039
Muhammad Muzzammil <muzzammil759@gmail.com>
2024-09-09 23:35:56 +0500

    Testimonial Section  Design completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import DiscussProject from "./components/DiscussProjects/DiscussProject";
 import Faqs from "./components/FAQs/Faqs";
 import Team from "./components/Team/Team";
 import Contact from "./components/Contact/Contact";
+import Testimonial from "./components/Testimonials/Testimonial";
 function App() {
   return (
     <>
@@ -16,6 +17,7 @@ function App() {
         <WhyUs />
         <DiscussProject />
         <Team />
+        <Testimonial />
         <Faqs />
         <Contact />
       </div>
